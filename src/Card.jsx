0928b2ef0d6b400546d19ebdb556e53d8716cd26
CodeSlice4Kivy <96576667+CodeSlice4Kivy@users.noreply.github.com>
2025-05-20 22:26:36 +0200

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@ function Card(){
     return(
         <div className="card">
             <img className="card-image" src={profilePic} alt="profie picture"></img>
-            <p>I amke Youtube vdieos and play video games</p>
+            <p>Becoming a pentagon believer</p>
         </div>
     );
 
