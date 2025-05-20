d50df30c4df2a3072df7639605e395e5ed0c36d3
CodeSlice4Kivy <96576667+CodeSlice4Kivy@users.noreply.github.com>
2025-05-20 22:25:40 +0200

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ function Header () {
 
     return(
         <header>
-            <h1>My Website</h1>
+            <h1>-</h1>
             <nav>
                 <ul>
                     <li> <a href="++"></a>-</li>
