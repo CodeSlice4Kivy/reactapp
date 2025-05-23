0c01b108d86f270d8ad963619a65efddca0468b7
CodeSlice4Kivy <96576667+CodeSlice4Kivy@users.noreply.github.com>
2025-05-23 18:56:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import Header from './Header.jsx'
+
 import Footer from './Footer.jsx'
-import Food from './Food.jsx'
-import Card from './Card.jsx'
 import PentagonCheckin from "./PentagonCheckin.jsx";
 
 function App() {
@@ -9,9 +7,6 @@ function App() {
     return(
         <>
             <PentagonCheckin/>
-            <Card/>
-            <Header/>
-            <Food/>
             <Footer/>
             
         </>
